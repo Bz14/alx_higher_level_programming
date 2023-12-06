ffe0f92b6ca0a930b384dbeaf50adb977371238b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-12-06 11:26:16 -0800

    File updated.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    maxScore = max(a_dictionary.values())
-    for key in a_dictionary:
-        if a_dictionary[key] == int(maxScore):
-            return key
+    maxScore = float('-inf')
+    student = None
+    for key, value in a_dictionary.items():
+        if value > maxScore:
+            maxScore = value
+            student = key
+    return student
