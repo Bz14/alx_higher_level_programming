607eb87d92fd7450ac52407eac6d0ef2daefa324
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-12-19 02:16:37 -0800

    File updated.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    """ This class defines an empty class that defines a square """
+    """
+    This class defines an empty class that defines a square
+    """
     pass
