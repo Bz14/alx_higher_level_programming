b0072469f369fb65f19bea91d531a774bddf5c0b
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-12-19 02:23:21 -0800

    File updated.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+"""Square Module"""
 class Square:
-    """
-    This class defines an empty class that defines a square
-    """
+    """This class defines an empty class that defines a square"""
     pass
