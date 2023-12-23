bda1cf739bd15be4925a05d09b5920e659e39561
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2023-12-23 11:39:45 -0800

    Getters and setters.

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Class for defining a square"""
+    def __init__(self, size=0):
+        """This is the init function."""
+        self.size = siz
+
+    @property
+    def size(self):
+        """Getter function for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
