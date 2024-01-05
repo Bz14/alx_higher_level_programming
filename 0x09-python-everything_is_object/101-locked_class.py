a3a97a19998531425b54071c0059706cd968d4b9
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-01-05 12:08:59 -0800

    File updated.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" Defines locked class."""
+
+
 class LockedClass:
+    """
+    Prevent object instantiation except for first_name attribute.
+    """
     __slots__ = ['first_name']
