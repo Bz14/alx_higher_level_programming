82914500327cb8eafe685d9e8139ae657a651aac
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-01-08 10:28:22 -0800

    Square.

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+""" A module to define a square """
+
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ A square class """
+    def __init__(self, size):
+        """ Initialize the size """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ An area function"""
+        return self.__size ** 2
