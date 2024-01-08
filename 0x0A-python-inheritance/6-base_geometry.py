edb6154f88532348071f92eb5a033d15a25444fd
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-01-08 04:36:53 -0800

    Improve Geometry.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A module to define a geometry class """
+
+
+class BaseGeometry:
+    """ An empty class """
+    def area(self):
+        """ An area function"""
+        raise Exception("area() is not implemented")
