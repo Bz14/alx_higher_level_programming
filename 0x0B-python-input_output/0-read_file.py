3a68c5ecd6dcf90211662ae5c6e2c0d101a8216d
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-01-09 01:12:27 -0800

    File updated.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,5 @@ def read_file(filename=""):
     """ A function to read from a file
     Args:
        filename: name of the file """
-    file_content = 'Hi'
     with open(filename, mode='r', encoding="utf-8") as f:
-        file_content = f.read()
-    print(file_content)
+        print(f.read())
