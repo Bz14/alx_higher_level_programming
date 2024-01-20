d812c3a2cd37aa15a7400a95f314539fb7c68885
eyubz <140620128+eyubz@users.noreply.github.com>
2024-01-20 07:44:44 +0300

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" A module to define a geometry class """
-
-
-class BaseGeometry:
-    """ An empty class """
-    pass
--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" A module to define a geometry class """
-
-
-class BaseGeometry:
-    """ An empty class """
-    def area(self):
-        """ An area function"""
-        raise Exception("area() is not implemented")
