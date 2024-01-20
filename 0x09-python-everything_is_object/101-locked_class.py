89c07bbae635f9e3a0b8cad6481562a4d290ce72
eyubz <140620128+eyubz@users.noreply.github.com>
2024-01-20 07:44:29 +0300

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Defines locked class."""
-
-
-class LockedClass:
-    """
-    Prevent object instantiation except for first_name attribute.
-    """
-    __slots__ = ['first_name']
--- 0x09-python-everything_is_object/19-copy_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def copy_list(orginal_list):
-    return orginal_list[:]
