6c9f540245fc0b6b21d01279b2aa3bdb48d866a7
eyubz <140620128+eyubz@users.noreply.github.com>
2024-01-20 07:44:58 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" A module to return the dictionary discription of some data structures. """
-
-
-def class_to_json(obj):
-    """ Function to return dictionary representation of an object """
-    return obj.__dict__
