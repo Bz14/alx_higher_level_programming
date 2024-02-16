2f3b0b1f7709950e6a04bde34aaa60dd4a435980
Eyerusalem <eyerusalembezu15@gmail.com>
2024-02-16 20:39:37 +0000

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+""" Define a locked class """
 class LockedClass:
+    """ Prevent the user from instantiating untill the attributes called first_name """
     __slots__ = ['first_name']
