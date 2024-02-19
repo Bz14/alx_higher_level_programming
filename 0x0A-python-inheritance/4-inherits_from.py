749b46f1fe6ac172a55480af7b0cbc2063dd0d0e
Eyerusalem <eyerusalembezu15@gmail.com>
2024-02-19 19:09:17 +0000

    File updated.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Check if object is an instance or not """
 
+
 def is_kind_of_class(obj, a_class):
     """ Return true if instance elase return false """
-    return isinstance(obj, a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
