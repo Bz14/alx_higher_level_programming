fd7bbce5111ba96deb5493a3e667490e4fd7c3ce
Eyerusalem <eyerusalembezu15@gmail.com>
2024-02-19 19:05:45 +0000

    Only subclass of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Check if object is an instance or not """
+
+def is_kind_of_class(obj, a_class):
+    """ Return true if instance elase return false """
+    return isinstance(obj, a_class)
