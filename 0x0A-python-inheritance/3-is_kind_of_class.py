4ecf2e0876a5bc66aa452422e54d2fd8a48cfcf6
Eyerusalem <eyerusalembezu15@gmail.com>
2024-02-19 19:00:40 +0000

    Same class or inherit from.

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Check whether instaince of an a class or not """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Return True if an instance else return False """
+    return isinstance(obj, a_class)
