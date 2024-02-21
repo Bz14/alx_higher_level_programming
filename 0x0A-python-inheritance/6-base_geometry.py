c45dab364a887169ad26eb5bfcc51c8b2441305e
Eyerusalem <eyerusalembezu15@gmail.com>
2024-02-21 18:42:06 +0000

    Improved geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" An improvement on basic geometry """
+
+
+class BaseGeometry:
+    """ Basic geometry class """
+    def area(self):
+        """ An a area function """
+        raise Exception("area() is not implemented")
