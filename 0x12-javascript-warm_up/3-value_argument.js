0c166801cb15861fed20276a80361bfbccad23cf
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-04-22 12:59:43 -0700

    File updated.

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const len = process.argv.length - 2;
-if (len === 0) {
+const args = process.argv.slice();
+if (args[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
