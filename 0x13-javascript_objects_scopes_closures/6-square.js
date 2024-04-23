6af68dd42639fa5357537e09446f6e180445b37f
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-04-23 04:57:10 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const Sq = require('./5-square');
+module.exports = class Square extends Sq {
   charPrint (c) {
-    if (!this.width || !this.height) {
-      return;
-    }
     if (c === undefined) {
       c = 'X';
     }
