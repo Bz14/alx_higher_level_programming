890c49d0ef14e29a0f38a74b14fa6076b53a49f2
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-04-24 13:15:48 -0700

    Occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const val of list) {
     if (val === searchElement) {
-	    count += 1;
+      count += 1;
     }
   }
   return count;
