50f3a55c852cf238de4d17c70320c951393a9f55
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-04-28 02:38:29 -0700

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Python script to get all states form a database """
+import sys
+import MySQLdb
+
+
+""" Function to list all states in a database """
+
+
+def list_states(username, password, database, search):
+    """ Fetch states form database """
+    conn = MySQLdb.connect(host="localhost",
+                           user=username,
+                           password=password,
+                           database=database,
+                           port=3306)
+    cursor = conn.cursor()
+    query = ("SELECT * FROM states WHERE name = '{}'"
+             " ORDER BY states.id ASC".format(search))
+    cursor.execute(query)
+    res = cursor.fetchall()
+    for val in res:
+        print(f"({val[0]}, '{val[1]}')")
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+    list_states(username, password, database, search)
