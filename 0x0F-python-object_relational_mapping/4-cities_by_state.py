3f4974a004be32ce83f4f9fa47a6263109141775
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-04-28 03:25:28 -0700

    File updated.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,9 +15,9 @@ def list_states(username, password, database):
                            database=database,
                            port=3306)
     cursor = conn.cursor()
-    query = ("SELECT cities.id, cities.name, states.name "
-             "FROM cities INNER JOIN states ON "
-             "cities.id = states.id ORDER BY cities.id ASC")
+    query = ("SELECT c.id, c.name, s.name "
+             "FROM cities as c INNER JOIN states as s ON "
+             "c.id = s.id ORDER BY c.id ASC")
     cursor.execute(query)
     res = cursor.fetchall()
     for val in res:
