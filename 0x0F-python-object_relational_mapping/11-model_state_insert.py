5378176989707c743be9cab99371e21d9d6a67ce
Eyerusalem Bezu <eyerusalembezu15@gmail.com>
2024-04-29 14:12:01 -0700

    Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" List states object from database """
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    dbName = sys.argv[3]
+    url = f"mysql://{user_name}:{password}"
+    url += f"@localhost:3306/{dbName}"
+    engine = create_engine(url)
+    Session = sessionmaker(engine)
+    session = Session()
+    obj = State(name="Louisiana")
+    session.add(obj)
+    session.commit()
+    print(obj.id)
