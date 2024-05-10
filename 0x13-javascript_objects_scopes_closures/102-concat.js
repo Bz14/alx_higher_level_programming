64b1b26c97254e7c74be03fc4b98fc915d629982
eyubz <140620128+eyubz@users.noreply.github.com>
2024-05-10 22:05:43 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const arr = process.argv;
-const data1 = fs.readFileSync(arr[2], 'utf8');
-const data2 = fs.readFileSync(arr[3], 'utf8');
-const concatenatedData = data1 + '\n' + data2 + '\n';
-fs.writeFileSync(arr[4], concatenatedData);
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const SquareParent = require('./5-square');
-module.exports = class Square extends SquareParent {
-  charPrint (c = 'X') {
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
-    }
-  }
-};
