57ebc1e114081bc27592334871fc627aba2ab556
Eyerusalem <eyerusalembezu15@gmail.com>
2024-06-20 08:00:26 +0000

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,21 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const args = process.argv.slice(2);
-fs.readFile(args[0], 'utf8', (err, firstContent) => {
-  if (err) {
-    console.log('Error');
-    return;
-  }
-  fs.readFile(args[1], 'utf8', (err, secondContent) => {
-    if (err) {
-      console.log('Error');
-      return;
-    }
-    const res = firstContent + '\n' + secondContent + '\n';
-    fs.writeFile(args[2], res, 'utf8', (err) => {
-      if (err) {
-        console.log('Error');
-      }
-    });
-  });
-});
+const firstContent = fs.readFileSync(args[0], 'utf8');
+const secondContent = fs.readFileSync(args[1], 'utf8');
+const res = `${firstContent}\n${secondContent}\n`;
+fs.writeFileSync(args[2], res, 'utf8');
