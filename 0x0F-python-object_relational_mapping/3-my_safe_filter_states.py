04b221f86cd833e2099ea510ee419bf7aaa725b3
Bz14 <eyerusalembezu15@gmail.com>
2024-06-24 09:01:24 +0000

    File updated.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,8 +5,12 @@ import sys
 
 
 if __name__ == "__main__":
-    db =  MySQLdb.connect(host="localhost", user=sys.argv[1],
-                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
     search = sys.argv[4]
     curr = db.cursor()
     curr.execute("SELECT * FROM states WHERE name LIKE %s", (search, ))
