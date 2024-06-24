1315fd7bc8e54aaba57e77d62424145ea6e33609
Bz14 <eyerusalembezu15@gmail.com>
2024-06-24 09:08:30 +0000

    File updated.

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,8 +5,12 @@ import sys
 
 
 if __name__ == "__main__":
-    db =  MySQLdb.connect(host="localhost", user=sys.argv[1],
-                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
     curr = db.cursor()
     curr.execute("""SELECT cities.name FROM
                    cities INNER JOIN states ON states.id=cities.state_id
