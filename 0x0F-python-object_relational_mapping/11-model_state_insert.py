b504cf4a5cb5499dfde97c2b7f3cc32a5a73ce1c
Bz14 <eyerusalembezu15@gmail.com>
2024-06-24 11:14:02 +0000

    Add a new state

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" List all states ordere by id """
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    n = sys.argv[4]
+    name = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(u, p, d)
+    engine = create_engine(name)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    obj = State(name="Louisiana")
+    session.add(obj)
+    session.commit()
+    print(obj.id)
+    session.close()
