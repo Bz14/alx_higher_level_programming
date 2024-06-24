89b508e5ff4013ace27ddbbf190f1a7684c2805b
Bz14 <eyerusalembezu15@gmail.com>
2024-06-24 09:58:39 +0000

    File updated.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""" List all states ordere by id """
+""" List all states ordere by id """
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,12 +9,12 @@ import sys
 if __name__ == "__main__":
     u = sys.argv[1]
     p = sys.argv[2]
-    d  = sys.argv[3]
+    d = sys.argv[3]
     name = f'mysql+mysqldb://{u}:{p}@localhost:3306/{d}'
     engine = create_engine(name, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    rows = session.query(State).order_by(States.id)
+    rows = session.query(State).order_by(State.id)
     for row in rows:
         print("{}: {}".format(row.id, row.name))
     session.close()
