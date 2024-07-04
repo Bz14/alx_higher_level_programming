f4740d9f349674f3c38408089cb61f304b524a9f
Bz14 <eyerusalembezu15@gmail.com>
2024-07-04 06:20:35 +0000

    File updated.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sending a request and displaying content
-response_byte=$(curl -s -o /dev/null -w '%{size_download}' "$1")
-echo "${response_byte}"
+echo "$(curl -s -o /dev/null -w '%{size_download}' "$1")"
