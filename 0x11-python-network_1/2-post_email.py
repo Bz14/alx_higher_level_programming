a8b2a2dad00cde36af21458175fafcd1a5aaf1d0
Bz14 <eyerusalembezu15@gmail.com>
2024-07-05 10:05:46 +0000

    File updated.

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     values = {"email": email}
-    data = urllib.parse.urlencode(values)
-    full_url = url + "?" + data
-    with urllib.request.urlopen(full_url) as response:
+    data = urllib.parse.urlencode(values).encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
         result = response.read()
-        print("Your email is: {}".format(result.decode('utf-8')))
+        print(result.decode('utf-8'))
