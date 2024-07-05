619a838ff6a2f38f32a78729dd3a8747d8be8894
Bz14 <eyerusalembezu15@gmail.com>
2024-07-05 10:25:18 +0000

    File updated.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Fetching a url  """
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
+    url = sys.argv[1]
     response = requests.get(url)
     usr_id = response.headers.get('X-Request-Id')
     print(usr_id)
