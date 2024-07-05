74b22a6b5405c47228b5cb289020c90ad010cb53
Bz14 <eyerusalembezu15@gmail.com>
2024-07-05 16:00:16 +0000

    My github.

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Fetching a url  """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    data = {"q": q}
+    try:
+        response = requests.post(url, data=data)
+        response_data = response.json()
+        if response_json:
+            print(("[{}] {}"
+                   .format(response_json['id'], response_json['name'])))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
